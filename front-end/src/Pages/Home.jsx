9589bf649c11c1a4c52508e4ff246b3ca53f03eb
Title <naruebet.pae@ku.th>
2022-10-03 22:10:33 +0700

    10-03-2022 22:09 -banner

--- front-end/src/Pages/Home.jsx
+++ front-end/src/Pages/Home.jsx
@@ -6,28 +6,25 @@ import { Button, Container } from "react-bootstrap";
 import { useAuthen } from "../services/Authen";
 
 import { Header } from "../components/Header";
-import { Banner } from "../Pages/Banner.jsx";
+import Banner from "../components/Banner";
+
 export const Home = () => {
   const isAuthen = useAuthen();
-  let bannerText = {
-    title: "Crypto-Bot Trade",
-    description: "บอทเทรดสำหรับคนที่ไม่ต้องการจ่องหน้าจอ ตามหลักกลยุทธ์ CDC และ EMA 10 21 บลาๆ เทสๆ",
-    img: "https://images.unsplash.com/photo-1606189934846-a527add8a77b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-  }
 
   return (
     <div>
       <Header />
-      <Container className="">
-        {isAuthen && <>Login</>}
-        <Banner />
+      <Banner />
 
-
-
-
-
-
+      <Container>
+        <h1>Hello</h1>
+        {/* <img
+          src="https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          class="img-fluid"
+          alt="picture investigation"
+        ></img> */}
       </Container>
+      {isAuthen && <>Login</>}
     </div>
   );
 };
