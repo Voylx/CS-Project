ebc8ab42cc056af2bf7e851cfb86f228465d6495
Voylx <channatt.m@ku.th>
2022-10-04 00:21:26 +0700

    10-04 00:20 add&del selected

--- back-end/src/routes/apibot.js
+++ back-end/src/routes/apibot.js
@@ -48,19 +48,25 @@ router.post("/delete", (req, res) => {
   //delete Bot in BOT TABLE
 });
 
-router.post("/addselected",
+router.post(
+  "/addselected",
   (req, res, next) => {
     const { Bot_id } = req.body;
 
-    Axios.post("/api/check/bot_by_botid", { Bot_id }, { headers: { Authorization: req.headers.authorization } })
+    Axios.post(
+      "/api/check/bot_by_botid",
+      { Bot_id },
+      { headers: { Authorization: req.headers.authorization } }
+    )
       .then((response) => {
-        req.body.botType = response?.data?.bot?.Type
+        req.body.botType = response?.data?.bot?.Type;
         next();
-      }).catch((error) => {
-        res.status(500).send(error?.response?.data)
       })
-
-  }, (req, res, next) => {
+      .catch((error) => {
+        res.status(500).send(error?.response?.data);
+      });
+  },
+  (req, res, next) => {
     const { Sym, Strategys_Id, botType, Amt_money } = req.body;
     if (botType) {
       if (!(Sym && Strategys_Id && Amt_money)) {
@@ -70,8 +76,8 @@ router.post("/addselected",
         });
         return;
       }
-    }
-    else {
+    } else {
+      req.body.Amt_money = null;
       if (!(Sym && Strategys_Id)) {
         res.status(400).send({
           status: "error",
@@ -81,17 +87,53 @@ router.post("/addselected",
       }
     }
     next();
-
-  }, (req, res) => {
+  },
+  (req, res) => {
     const { User_id, Bot_id, Sym, Strategys_Id, botType, Amt_money } = req.body;
-    db.execute("", [], function (err, res) {
+    // const Amt_money = req.body.Amt_money || null;
+    db.execute(
+      "INSERT IGNORE INTO selected (Bot_id, Sym, Strategys_Id, Amt_money) VALUES (?,?,?,?)",
+      [Bot_id, Sym, Strategys_Id, Amt_money],
+      function (err, result) {
+        if (err) {
+          console.error(err);
+          res.status(500).send({ status: "error", message: err.sqlMessage });
+          return;
+        }
+        res.status(200).send({
+          status: "success",
+          result,
+        });
+      }
+    );
+    console.log({ User_id, Bot_id, Sym, Strategys_Id, botType, Amt_money });
+    // res.send(req.body);
+  }
+);
+
+router.post("/delselected", (req, res) => {
+  const { Bot_id, Sym, Strategys_Id } = req.body;
+  if (!(Bot_id && Sym && Strategys_Id)) {
+    res.status(400).send({
+      status: "error",
+      message: "Incomplete request ",
+    });
+    return;
+  }
+  db.execute(
+    "DELETE FROM selected WHERE Bot_id = ? AND Sym = ? AND Strategys_Id = ?",
+    [Bot_id, Sym, Strategys_Id],
+    function (err, results) {
       if (err) {
         console.error(err);
         res.status(500).send({ status: "error", message: err.sqlMessage });
       }
-    })
-    console.log({ User_id, Bot_id, Sym, Strategys_Id, botType, Amt_money });
-    res.send(req.body)
-  })
+      res.status(200).send({
+        status: "success",
+        results,
+      });
+    }
+  );
+});
 
 module.exports = router;
