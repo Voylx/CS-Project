ea639620027597d52b63863937812ace78481fc2
Voylx <channatt.m@ku.th>
2022-10-14 15:27:58 +0700

    10-14 3:27

--- front-end/src/components/LineBotDetail.jsx
+++ front-end/src/components/LineBotDetail.jsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Axios from "../services/Axios";
 import { Link, useNavigate } from "react-router-dom";
-import { Container, Form, Button, Alert } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
 
+import RealtimeBoxGroup from "./RealtimeBoxGroup";
+import SelectStrategies from "./SelectStrategies";
 import { useAuthen } from "../services/Authen";
 
-export const LineBotDetail = () => {
+export const LineBotDetail = (props) => {
   let navigate = useNavigate();
   async function checkLinkAPI() {
     try {
       const res = await Axios.post("/api/check/link_line", {});
 
-      console.log(res.data);
+      // console.log(res.data);
       if (!res.data.linkLine) {
         navigate("/bot/linkline");
       }
@@ -26,30 +28,14 @@ export const LineBotDetail = () => {
 
   return (
     <div>
-      <Container fluid="md">
-        <div
-          className="border p-3 mx-auto mt-5 col-lg-6 col-md-8 shadow-lg"
-          style={{ borderRadius: "11px" }}
-        >
-          <div className="d-flex justify-content-between align-items-center">
-            <h2 className="ms-7">Line Notification</h2>
-            <h6
-              className="me-2 text-secondary"
-              onClick={() => navigate("/bot")}
-            >
-              {"Go back"}
-            </h6>
-          </div>
-          <div className="mb-5 text-center">
-            <div className="fs-3">หน้าเชื่อมแล้ว</div>
-            <div
-              className=" fs-2 mx-auto mt-2 shadow-lg "
-              // style={{ borderRadius: "11px", height: "10rem" }}
-            >
-              random
-            </div>
-          </div>
-        </div>
+      <Container>
+        <Row className="m-4 g-3 ">
+          <h2 className="ms-7 ">Viewer Trade</h2>
+        </Row>
+        <RealtimeBoxGroup />
+        <div className="linetext mb-5 text-muted"></div>
+        <SelectStrategies {...props} />
+        <div style={{ height: "10rem" }}>.</div>
       </Container>
     </div>
   );
